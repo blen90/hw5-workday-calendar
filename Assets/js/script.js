e42181b803bf97b8010201ed54a0f269a646365a
Blenda Orellana <blen.or90@gmail.com>
2021-06-19 14:40:41 -0500

    added currenthour var

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,18 @@
+var hourEl;
+const timeBlock = $("#timeBlock");
+
+// Set currentDayEl to display currentDay in the header
 var currentDayEl = moment();
 $("#currentDay").text(currentDayEl.format("dddd, MMMM Do"));
+
+//What hour of the day it is
+var currentHour = moment().format("hh:" + "mm" + "a");
+console.log(currentHour);
+
+
+
+
+
+    
+
 
